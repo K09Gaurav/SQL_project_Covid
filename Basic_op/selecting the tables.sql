179e90ffc3ac8d785c0561a1cf070e35f4ec20ea
K09Gaurav <k09gaurav@gmail.com>
2024-01-01 23:37:05 +0530

    Edited Selecting basic

--- Basic_op/selecting the tables.sql
+++ Basic_op/selecting the tables.sql
@@ -1,3 +1,4 @@
 -- Practcing basic sql select query
+-- Ordered in ascending order according to Country name and then date
 select * from SQL_COVID..Deaths Order by location, date;
 select * from SQL_COVID..Vaccine Order by location, date;
